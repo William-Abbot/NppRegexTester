73d244359e36e137c971a69daa7623af7aaa297b
William Abbot <59292592+William-Abbot@users.noreply.github.com>
2022-08-16 20:19:29 -0500

    Add files via upload

---
+++ NppRegexTester/src/DockingFeature/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by regexDialog.rc
+//
+#define IDD_PROPPAGE_SMALL              102
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_EDIT3                       1003
+#define IDC_BUTTON1                     1004
+#define IDC_BUTTON2                     1005
+#define IDC_CHECK1                      1006
+#define IDC_BUTTON3                     1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
